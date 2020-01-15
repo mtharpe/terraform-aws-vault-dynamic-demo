ef8d041bd68d786d6da0d416c773785881333f04
Michael Tharpe <mtharpe@gmail.com>
2020-01-14 20:50:12 -0500

    Update region

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "vault_aws_access_credentials" "creds" {
 provider "aws" {
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 data "aws_ami" "ubuntu" {
